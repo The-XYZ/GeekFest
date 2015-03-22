68409137a576615181397343de9636ee80a5cb79
naman14 <namandwivedi14@gmail.com>
2015-03-22 10:09:36 +0530

    chart of nutritions

--- app/src/main/java/com/xyz/geekfest/MainFragment.java
+++ app/src/main/java/com/xyz/geekfest/MainFragment.java
@@ -515,7 +515,7 @@ public class MainFragment  extends ScrollTabHolderFragment implements AbsListVie
 //            Log.d("size", row.text);
 
             holder.ingName.setText(row.cname);
-            holder.ingPrice.setText(row.cprice);
+            holder.ingPrice.setText("₹"+row.cprice);
             holder.ingNV.setText(row.cvalue);
 
             // image
